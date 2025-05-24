17de3555b69884e5b666d1b436c60b7c55d3e783
CORP\bf105320 <borja.ferrervidal@qorvo.com>
2025-05-24 23:06:45 +0200

    Add project files.

---
+++ qNotePlayer/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using qNotePlayer;
+
+Console.WriteLine("Hola Adriana!!");
+Console.ReadKey();
+// Create an instance of qPlayer
+qPlayer player = new qPlayer();
+
+// Use the instance to call the PlayNote method
+//player.PlayNote(440f, 1000);
+//player.PlayNote(523.25f, 1000);
+
+bool running = true;
+while (running)
+{
+    var keyInfo = Console.ReadKey(true);
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.A:
+            player.PlayNote(261.63f, 500); // C4
+            break;
+        case ConsoleKey.S:
+            player.PlayNote(293.66f, 500); // D4
+            break;
+        case ConsoleKey.D:
+            player.PlayNote(329.63f, 500); // E4
+            break;
+        case ConsoleKey.F:
+            player.PlayNote(349.23f, 500); // F4
+            break;
+        case ConsoleKey.G:
+            player.PlayNote(392.00f, 500); // G4
+            break;
+        case ConsoleKey.H:
+            player.PlayNote(440.00f, 500); // A4
+            break;
+        case ConsoleKey.J:
+            player.PlayNote(493.88f, 500); // B4
+            break;
+        case ConsoleKey.Escape:
+            running = false;
+            break;
+    }
+}
+
---
+++ qNotePlayer/qPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace qNotePlayer
+{
+    public class qPlayer
+    {
+
+        private int durationMs = 500; // Duración por defecto en milisegundos
+        public void PlayNote(float frequency, int durationMs)
+        {
+            // Generador de señal senoidal
+            var signalGenerator = new SignalGenerator()
+            {
+                Gain = 0.2, // Volumen
+                Frequency = frequency, // Frecuencia de la nota
+                Type = SignalGeneratorType.Sin // Onda senoidal
+            };
+
+            // Limita la duración de la señal
+            var note = signalGenerator.Take(TimeSpan.FromMilliseconds(durationMs));
+
+            // Crea una salida de audio
+            using (var waveOut = new WaveOutEvent())
+            {
+                waveOut.Init(note);
+                waveOut.Play();
+
+                // Espera hasta que termine de reproducirse
+                Thread.Sleep(durationMs);
+            }
+        }
+
+        public float CalculateFrequency(int noteNumber)
+        {
+            // Fórmula para calcular la frecuencia de una nota MIDI
+            // A4 = 440 Hz es la nota 69 en MIDI
+            float frequency = 440.0f * (float)Math.Pow(2.0, (noteNumber - 69) / 12.0);
+            return frequency;
+        }
+    }
+}
